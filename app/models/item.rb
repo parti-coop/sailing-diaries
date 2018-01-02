5063f1dde9352da77cad38bff75fac72a9fee2c9
ganguri <ganguri@parti.xyz>
2018-01-02 15:02:07 +0900

    2018년으로 넘어오면서 생기는 오류를 수정합니다

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,15 +2,15 @@ class Item < ApplicationRecord
   has_many :emotions
 
   def self.count_emotions(month, emotion_title)
-    Emotion.where(title: emotion_title).where(item_id: Item.by_month(month)).count
+    Emotion.where(title: emotion_title).where(item_id: Item.by_month(month, year: 2017)).count
   end
 
   def self.count_king_emotions(month)
-    Emotion.where(item_id: Item.by_month(month)).group(:title).count.values.max
+    Emotion.where(item_id: Item.by_month(month, year: 2017)).group(:title).count.values.max
   end
 
   def self.count_month(month)
-    Emotion.where(item_id: Item.by_month(month)).count
+    Emotion.where(item_id: Item.by_month(month, year: 2017)).count
   end
 
   def self.sizes(total, month)
